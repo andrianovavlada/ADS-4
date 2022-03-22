fd172ccc048ec0812e3e91789b7903e3d86b54cc
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-03-22 22:25:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,12 @@ int countPairs1(int *arr, int len, int value) {
     return pairs;
 }
 int countPairs2(int *arr, int len, int value) {
-  int pairs = 0;
+    int pairs = 0;
+    int i = len - 1;
+    while (arr[i] > value) {
+        len--;
+        i--;
+    }
     for (int i = 0; i < len - 1; i++) {
         for (int j = len - 1; j > i; j--) {
             if (arr[i] + arr[j] == value) {
@@ -21,29 +26,33 @@ int countPairs2(int *arr, int len, int value) {
     }
     return pairs;
 }
-int cbinsearch(int *arr, int size, int value) {
-    int y = 0, i = 0, j = size - 1;
-    int mid = i + (j - i) / 2;
-    if (true) {
-        for (int i = 0; i < mid; i++) {
-            if (arr[i] == value)
-                y++;
+int cbinsearch(int *arr, int size, int valueint value, int l) {
+   int pairs = 0, i = l, j = size - 1, mid = 0;
+    while (i < j) {
+        mid = i + (j - i) / 2;
+       if (arr[mid] == value && mid != l) {
+            pairs++;
+            int r = mid, l = mid;
+               while (arr[r--] == value && r > l) {
+                    pairs++;
+                }
+                while (arr[l++] == value) {
+                    pairs++;
+                }
+                return pairs;
+        } else if (arr[mid] > value) {
+            j = mid - 1;
+        } else {
+            i = mid + 1;
         }
-        for (int j = size; j >= mid; j--) {
-            if (arr[j] == value)
-                y++;
-        }
-        return y;
-    } else {
+     }
         return 0;
     }
-}
-
 int countPairs3(int *arr, int len, int value) {
     int pairs = 0;
     int i = 0;
     while (i < len) {
-        pairs += cbinsearch(arr, len, value - arr[0]);
+        pairs += cbinsearch(arr, len, value - arr[i], i);
         i++;
         arr++;
     }
